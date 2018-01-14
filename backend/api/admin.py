5ff0784e4a64455650adfabd298524829aa04abc
Matt Wiens <mwiens91@gmail.com>
2018-01-14 00:37:17 -0800

    Give admin interface more info about models

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from api.models import Camera, Child, Event
 
-admin.site.register(Camera)
-admin.site.register(Child)
+class CameraAdmin(admin.ModelAdmin):
+    """Interface modifiers for the Camera model for the admin page."""
+    list_display = ('name', 'cameraid', 'key', 'user')
+
+class ChildAdmin(admin.ModelAdmin):
+    """Interface modifiers for the Camera model for the admin page."""
+    list_display = ('first_name', 'last_name', 'sex', 'user')
+
+admin.site.register(Camera, CameraAdmin)
+admin.site.register(Child, ChildAdmin)
 admin.site.register(Event)
