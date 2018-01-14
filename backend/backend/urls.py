69551107732a41a47006ec8e27bf520781cbc501
Matt Wiens <mwiens91@gmail.com>
2018-01-13 17:09:57 -0800

    Add camera registration view

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import api.views as api_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('<int:parentid>/camera/register/', api_views.register_camera),
 ]
